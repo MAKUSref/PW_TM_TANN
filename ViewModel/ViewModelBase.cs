37321d4f721e31d2fac4c18b3e8463c8fce4811e
bakuganmw <bakuganmw1@gmail.com>
2022-04-28 22:06:55 +0200

    ViewModel

--- ViewModel/ViewModelBase.cs
+++ ViewModel/ViewModelBase.cs
@@ -4,26 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
+using Logic;
 namespace ViewModel
 {
     public class ViewModelBase
     {
+        
         private int _ballsNumber;
         private readonly Pmodel _pmodel;
-
+        private List<Ball> _balls;
         public RelayCommand RelayCommand { get; set; }
         public ViewModelBase()
         {
             this.RelayCommand = new RelayCommand(this);
         }
+        private Pmodel model { get; set; }
+       
         public void ShowBalls()
         {
-            Console.WriteLine("Hello World");
+            _balls = _pmodel.Balls(_ballsNumber,800,100);
+            _pmodel.StartAnimation(_balls);
 
         }
 
-        private Pmodel model { get; set; }
-        
+        public void StopBalls()
+        {
+            _pmodel.StopAnimation();
+
+        }
+
+
     }
 
 }

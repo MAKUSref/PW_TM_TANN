b622d45540bb9125747866d7dd6ebb87a21a58f4
bakuganmw <bakuganmw1@gmail.com>
2022-04-28 06:40:31 +0200

    add Icommand

---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class ViewModelBase
+    {
+        public RelayCommand RelayCommand { get; set; }
+        public ViewModelBase()
+        {
+            this.RelayCommand = new RelayCommand(this);
+        }
+        public void ShowBalls()
+        {
+            Console.WriteLine("Hello World");
+
+        }
+    }
+
+}

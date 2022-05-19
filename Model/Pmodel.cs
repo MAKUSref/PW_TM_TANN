d48fcbd42c69f3bc724e15d1c8cbb9156ca145e6
bakuganmw <bakuganmw1@gmail.com>
2022-05-19 23:08:51 +0200

    Task2: create model

--- Model/Pmodel.cs
+++ Model/Pmodel.cs
@@ -6,30 +6,10 @@ namespace Model
     public abstract class Pmodel
     {
 
-        public abstract List<Ball> Balls(int ballNumber, int rectWidth, int rectHeight);
-
-        public abstract Movement CreateMovement(List<Ball> balls);
-        public abstract Task StartAnimation(Movement instance, List<Ball> balls); //ruch
-
-        public abstract void StopAnimation(Movement instance);   //zatrzymanie
-
-        public static Pmodel CreateApi()
-        {
-            return new ModelAPI();
-        }
 
     }
     public class ModelAPI : Pmodel
     {
-        public override List<Ball> Balls(int ballNumber, int rectWidth, int rectHeight)
-        => BallFactory.CreateBalls(ballNumber, rectWidth, rectHeight);
-
-        public override Movement CreateMovement(List<Ball> balls) => new Movement(balls);
-
-
-        public override async Task StartAnimation(Movement instance, List<Ball> balls)
-        => await instance.StartMoving();
-
-        public override void StopAnimation(Movement instance) => instance.StopMoving();
+        
     }
 }
